02bde05c20857f573d5c573fbdb983c5bd8e8f24
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-24 10:42:06 +0530

    server.js path correct

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.static(path.join(__dirname, "dist", "angular-login-ngrx")));
 
 app.get("/*", (req, res, next) => {
-  return res.sendFile(path.join(__dirname, "dist", "angular-login-ngrx", "index.html"));
+  console.log(
+    "Path:",
+    path.join(__dirname, "dist", "angular-login-ngrx", "index.html")
+  );
+  return res.sendFile(
+    path.join(__dirname, "dist", "angular-login-ngrx", "index.html")
+  );
 });
 
 app.listen(process.env.PORT || 3000);
